6c7e6a7d5b4b57f22f0ae2a027f983ea1ee02f4e
Azamat Pulatov <108136459+az1kkRich@users.noreply.github.com>
2023-11-08 13:34:03 +0500

    Update olim.py

--- olim.py
+++ olim.py
@@ -1,6 +1,4 @@
 
-# 3-masala
-
 n = int(input())
 n_banknota = list(map(int, input().split()))
 mijoz = int(input())
@@ -12,15 +10,19 @@ summma = sum(n_banknota)
 max_bank = max(n_banknota)
 
 for i in qarz:
-    if i in n_banknota:
+    if i == 0:
+        res.append(0)
+    elif i in n_banknota:
         res.append(1)
     
+    
     elif summma - i in n_banknota:
-        res.append(n-1)
+        res.append(len(n_banknota)-1)
     elif i == summma:
-        res.append(n)
+        res.append(len(n_banknota))
     elif i > summma:
         res.append(-1)
+        
     elif i > max_bank:
         for j in n_banknota:
             if i-j in n_banknota:
@@ -28,14 +30,12 @@ for i in qarz:
                 break
     
         
-    # elif i not in n_banknota or summma - i not in n_banknota or i != summma or i<max_bank:
-    #     res.append(-1)
-    else :
+    elif i not in n_banknota or summma - i not in n_banknota or i != summma or i<max_bank:
         res.append(-1)
+    # else :
+    #     res.append(-1)
     
-    
-      
+        
         
 for j in res:
     print(j, end=' ')
-
